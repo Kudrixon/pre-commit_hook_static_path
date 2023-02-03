c76cdad6a001d649794ef1bee71133be9f6c3f63
Przemyslaw Kudriawcew <pkudriaw@cisco.com>
2023-02-03 06:30:00 -0800

    Edit setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='check-static-path',
-    version='1.0.2',
+    version='1.0.3',
     packages=find_packages(include=['check-static-path', 'check-static-path.*'])
 )
