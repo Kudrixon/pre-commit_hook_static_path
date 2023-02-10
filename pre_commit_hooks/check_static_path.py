481b55960c1367de5e97f71ec00f078181fe5569
Przemyslaw Kudriawcew <pkudriaw@cisco.com>
2023-02-10 09:06:40 -0800

    Remove text mode from subprocess not compatible with python 3.6

--- pre_commit_hooks/check_static_path.py
+++ pre_commit_hooks/check_static_path.py
@@ -67,14 +67,14 @@ def main(argv: Union[Sequence[str], None] = None) -> int:
     retval = 0
 
     if(args.nodiff == "false"):
-        text = subprocess.run(['git', 'diff', "--unified=0", 'HEAD'], stdout=subprocess.PIPE, text=True).stdout
+        text = subprocess.check_output(['git', 'diff', "--unified=0", 'HEAD'], universal_newlines=True)
         matches = extractMatches(text, pattern)
         filenames = [match.replace("+++ b/", "") for match in matches if match.startswith("+++ b/")]
         filenames = [filename for filename in filenames if filename is not None]
         results = findStringInRange(filenames, args.keywords)
         if results:
             print(f"Forbidden static strings: {args.keywords}\n")
-            
+
         for r in results:
             print(f"Static path detected in file {r[0]}, line {r[1]} content: {r[2]}, change to dynamic needed")
             retval = 1
